5b3ba906929e490d491e9860f280386ea8a287cc
Austin Wallace <auswallace@outlook.com>
2022-04-22 11:12:29 -0500

    Styling changes and added working contact form

--- SAT.MVC.UI/App_Start/BundleConfig.cs
+++ SAT.MVC.UI/App_Start/BundleConfig.cs
@@ -7,8 +7,8 @@ namespace SAT.MVC.UI
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -18,13 +18,19 @@ namespace SAT.MVC.UI
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/css/styles.css",
+                      "~/Content/css/Custom.css"
+                      ));
+            bundles.Add(new ScriptBundle("~/bundles/template").Include(
+                    "~/Content/scripts.js"
+                ));
         }
     }
 }
